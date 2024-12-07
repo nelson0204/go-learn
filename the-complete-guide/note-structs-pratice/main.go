bdae123f2bb2189f4f923bc8c1833873337d866e
nelsonjr <nelson0204@gmail.com>
2024-12-07 00:04:08 +0000

    Write struct with tag in json file

---
+++ the-complete-guide/note-structs-pratice/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"example/note/note"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("SAving the note failed.")
+	}
+
+	fmt.Println("Save the note succeeded!")
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title: ")
+	content := getUserInput("Note content: ")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
